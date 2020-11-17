087e2e8713f2c4e986f6c59174ec78339df036af
Francesco <francescofacca@gmail.com>
2020-11-17 14:40:13 +0000

    Step 7 - Attach a database to a web application.

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,9 @@
+require 'pg'
+
 class Bookmark
   def self.all
-    [
-      "http://www.makersacademy.com",
-      "https://www.codewars.com",
-      "http://www.google.com"
-     ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec('SELECT * FROM bookmarks')
+    result.map { |bookmark| bookmark['url'] }
   end
 end
